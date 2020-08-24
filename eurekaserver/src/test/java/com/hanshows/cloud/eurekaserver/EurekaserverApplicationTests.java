ca6e3fef5e0c3adc1a213b7db95e7702a03bcda1
876598055 <39091751+876598055@users.noreply.github.com>
2020-08-24 13:00:55 +0800

    demo、 demo

---
+++ eurekaserver/src/test/java/com/hanshows/cloud/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanshows.cloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
